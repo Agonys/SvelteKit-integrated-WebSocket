ca66675ffe8c41b83768900ffebfb7162757370b
Agonys <piotr.chmura325@gmail.com>
2023-10-15 01:06:49 +0200

    cloudflare adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,8 +6,17 @@ declare global {
 		interface Locals {
 			wss?: ExtendedWebSocketServer;
 		}
+
 		// interface PageData {}
-		// interface Platform {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
 	}
 }
 
