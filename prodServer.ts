4271d5b208908c9b1a6d873c433424203378184e
Agonys <piotr.chmura325@gmail.com>
2023-10-15 16:23:39 +0200

    cloudflare directory check

--- prodServer.ts
+++ prodServer.ts
@@ -1,9 +1,22 @@
 import * as path from 'node:path';
 import * as url from 'node:url';
+import * as fs from 'node:fs';
 import { createWSSGlobalInstance, onHttpServerUpgrade } from './src/lib/server/webSocketUtils';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+console.log(
+	JSON.stringify(
+		{
+			filename: __filename,
+			dirname: __dirname,
+			files: fs.readdirSync(__dirname)
+		},
+		null,
+		2
+	)
+);
 
 createWSSGlobalInstance();
 
